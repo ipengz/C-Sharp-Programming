87e8af1d85e9261f4c7cf80434cbde63b9c1c9d2
ipengz <deylonku@gmail.com>
2024-09-19 15:27:43 +0800

    Add files via upload

---
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Channels;
+
+namespace InheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.MakeSound();
+
+            Cat myCat = new Cat();
+            myCat.MakeSound();
+            
+            Console.ReadKey();
+        }
+    }
+
+    //Base Class (Parent/Super Class)
+    class Animal
+    {
+        public void Eat() 
+        {
+            Console.WriteLine("Eating...");
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal makes generic sound");
+        }
+    }
+
+    //Derived Class (Child class or Subclass)
+    //The class that ineherits the members of the base class
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meaowing...");
+        }
+    }
+
+    //A breed of Dog
+    class Collie : Dog
+    {
+        public void GoingNuts()
+        {
+            Console.WriteLine("Collie going nuts");
+        }
+    }
+
+}
---
+++ InheritanceApp2/InheritanceApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace InheritanceApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee dylon = new Employee("Dylon", 29, "Senior Exec", 13671);
+            dylon.DisplayEmployeeInfo();
+            Console.ReadKey();
+        }
+
+    }
+
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, int age) 
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine("Person constructor called");
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    public class Employee : Person 
+    {
+        public string JobTitle { get; private set; }
+
+        public int EmployeeID { get; private set; }
+        public Employee(string name, int age, string jobTitle, int employeeID)
+            : base(name,age)
+        {
+            JobTitle = jobTitle;
+            EmployeeID = employeeID;
+            Console.WriteLine("Employee constructor called");
+        }
+
+        public void DisplayEmployeeInfo() 
+        {
+            DisplayPersonInfo();
+            Console.WriteLine($"Job Title: {JobTitle}, Employee ID: {EmployeeID}");
+        }
+    }
+}
